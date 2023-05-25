aabdce1fc77ed3143e87b0a1aabf7f97eb28bd28
bambee <124007041+bambee83@users.noreply.github.com>
2023-05-25 21:10:11 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.08 ms, Memory: 75.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120812. 최빈값 구하기/최빈값 구하기.java
+++ 프로그래머스/lv0/120812. 최빈값 구하기/최빈값 구하기.java
@@ -1,37 +1,21 @@
-import java.util.HashMap;
-import java.util.Map;
-
 class Solution {
-public int solution(int[] array) {
-  // 최빈값의 빈도수를 저장할 Map 객체 생성
-  Map<Integer, Integer> map = new HashMap<>();
+    public int solution(int[] array) {
+        int answer = -1;
+        int[] num = new int[2000];
+        for(int i = 0; i < array.length; i++){
+            num[array[i] + 1000]++;
+        }
 
-  // 배열의 각 요소를 Map 객체에 추가
-  for (int i = 0; i < array.length; i++) {
-    map.put(array[i], map.getOrDefault(array[i], 0) + 1);
-  }
-
-  // 최빈값의 빈도수
-  int maxCount = 0;
-
-  // 최빈값
-  int mode = -1;
-
-  // Map 객체를 순회하며 최빈값을 찾는다.
-  for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-    if (entry.getValue() > maxCount) {
-      maxCount = entry.getValue();
-      mode = entry.getKey();
+        int max = 0;
+        for(int i = 0; i < 2000; i++){
+            if(num[i] > max){
+                max = num[i];
+                answer = i - 1000;
+            }
+            else if(num[i] == max){
+                answer = -1;
+            }
+        }
+        return answer;
     }
-  }
-
-  // 최빈값이 여러 개면 -1을 반환한다.
-  for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-    if (entry.getValue() == maxCount && entry.getKey() != mode) {
-      return -1;
-    }
-  }
-
-  return mode;
-}
 }
