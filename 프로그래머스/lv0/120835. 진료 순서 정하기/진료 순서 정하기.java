20ca6dea7e759a7dae79cc48dbb47d68a35e006d
bambee <124007041+bambee83@users.noreply.github.com>
2023-05-30 00:10:44 +0900

    [level 0] Title: 진료 순서 정하기, Time: 0.02 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/lv0/120835. 진료 순서 정하기/진료 순서 정하기.java
+++ 프로그래머스/lv0/120835. 진료 순서 정하기/진료 순서 정하기.java
@@ -2,25 +2,17 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] emergency) {
-      // 복사 배열 생성
-        int[] sortedEmergency = Arrays.copyOf(emergency, emergency.length);
+    int[] answer = new int[emergency.length];
 
-        // 복사 배열을 내림차순으로 정렬
-        Arrays.sort(sortedEmergency);
-
-        // 결과 배열 생성
-        int[] result = new int[emergency.length];
-
-        for (int i = 0; i < emergency.length; i++) {
-            for (int j = 0; j < sortedEmergency.length; j++) {
-                // 원본 배열의 요소가 복사 배열에서 어디에 있는지 찾아 결과 배열에 넣는다.
-                // 이때, 응급도가 높은 순서대로 낮은 인덱스 값을 부여한다.
-                if (emergency[i] == sortedEmergency[j]) {
-                    result[i] = sortedEmergency.length - j;
+        for(int i = 0; i < answer.length; i++){
+            int idx = 1;
+            for(int j = 0; j < answer.length; j++){
+                if(emergency[i] < emergency[j]){
+                    idx++;
                 }
             }
+            answer[i] = idx;
         }
-
-        return result;
+        return answer;
     }
 }
