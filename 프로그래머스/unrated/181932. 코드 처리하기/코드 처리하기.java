5fccacfd65c8a884af44e46ba6878a72849641c5
bambee <124007041+bambee83@users.noreply.github.com>
2023-05-30 00:58:42 +0900

    [unrated] Title: 코드 처리하기, Time: 0.03 ms, Memory: 72.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181932. 코드 처리하기/코드 처리하기.java
+++ 프로그래머스/unrated/181932. 코드 처리하기/코드 처리하기.java
@@ -7,7 +7,8 @@ class Solution {
             if (ch == '1') {
                 mode = 1 - mode;
             } else {
-                if ((idx % 2 == 0 && mode == 0) || (idx % 2 == 1 && mode == 1))                 {ret.append(ch); }
+                if ((idx % 2 == 0 && mode == 0) || (idx % 2 == 1 && mode == 1))                
+                {ret.append(ch); }
             }
         }
         return ret.length() > 0 ? ret.toString() : "EMPTY";
