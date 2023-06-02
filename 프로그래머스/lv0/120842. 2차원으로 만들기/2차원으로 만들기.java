128bc243c169d533709118952c2f9430bfee0340
bambee <124007041+bambee83@users.noreply.github.com>
2023-06-03 03:21:03 +0900

    [level 0] Title: 2차원으로 만들기, Time: 0.01 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120842. 2차원으로 만들기/2차원으로 만들기.java
+++ 프로그래머스/lv0/120842. 2차원으로 만들기/2차원으로 만들기.java
@@ -3,6 +3,7 @@ class Solution {
         int size = num_list.length;
         int[][] answer = new int[size/n][n];
         for (int i = 0; i< size; i++) {
+            // 2차원 배열의 행 인덱스는 i/n으로, 열 인덱스는 i%n으로 계산
             answer[i/n][i%n]=num_list[i];
         }
         return answer;
