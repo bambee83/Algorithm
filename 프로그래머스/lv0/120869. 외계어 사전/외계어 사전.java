4fdeb2bc7e62f0248d078f68a364611b9971e3fb
bambee <124007041+bambee83@users.noreply.github.com>
2023-06-18 20:57:33 +0900

    [level 0] Title: 외계어 사전, Time: 0.04 ms, Memory: 78.9 MB -BaekjoonHub

--- 프로그래머스/lv0/120869. 외계어 사전/외계어 사전.java
+++ 프로그래머스/lv0/120869. 외계어 사전/외계어 사전.java
@@ -1,39 +1,12 @@
-import java.util.*;
-
 class Solution {
     public int solution(String[] spell, String[] dic) {
-        List<String> permutations = new ArrayList<>();
-        permutation(spell, 0, spell.length, permutations);
-
-        for (String word : dic) {
-            if (permutations.contains(word)) {
-                return 1;
+        for(int i=0;i<dic.length;i++){
+            int answer = 0;
+            for(int j=0;j<spell.length;j++){
+                if(dic[i].contains(spell[j])) answer ++;
             }
+            if(answer==spell.length) return 1;
         }
-        
         return 2;
-    }
-
-    private void permutation(String[] arr, int depth, int n, List<String> result) {
-        if (depth == n) {
-            StringBuilder sb = new StringBuilder();
-            for (String s : arr) {
-                sb.append(s);
-            }
-            result.add(sb.toString());
-            return;
-        }
-
-        for (int i = depth; i < n; i++) {
-            swap(arr, i, depth);
-            permutation(arr, depth + 1, n, result);
-            swap(arr, i, depth);
-        }
-    }
-
-    private void swap(String[] arr, int i, int j) {
-        String temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
     }
 }
