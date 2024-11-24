1cdd5688501153873293632b3a1a0e12358267a6
bambee <124007041+bambee83@users.noreply.github.com>
2024-11-24 19:06:47 +0900

    [Bronze V] Title: A+B - 3, Time: 188 ms, Memory: 17932 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int n2= sc.nextInt();
+
+        for (int n = 0 ; n < n2 ; n++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            System.out.println(a + b);
+        }
+
+    }
+}
+
