f90a96a0425196d00c1e70bbf0afdab09b51569b
bambee <124007041+bambee83@users.noreply.github.com>
2024-11-24 20:46:03 +0900

    [Bronze V] Title: A+B - 5, Time: 120 ms, Memory: 14620 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
@@ -0,0 +1,26 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        while (true) {
+            String[] input = br.readLine().split(" ");
+            int A = Integer.parseInt(input[0]);
+            int B = Integer.parseInt(input[1]);
+
+            if (A == 0 && B == 0) {
+                break;
+            }
+            bw.write((A + B) + "\n");
+        }
+
+        // 출력 플러시 및 스트림 닫기
+        bw.flush();
+        bw.close();
+        br.close();
+
+    }
+}
+
