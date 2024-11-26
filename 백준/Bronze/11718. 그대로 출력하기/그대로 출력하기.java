be45ae2f7f693dc82f01deb52e8beb75091048e0
bambee <124007041+bambee83@users.noreply.github.com>
2024-11-26 10:51:04 +0900

    [Bronze III] Title: 그대로 출력하기, Time: 196 ms, Memory: 17924 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args)  throws IOException {
+        Scanner sc = new Scanner(System.in);
+
+        while (sc.hasNextLine()) { // 입력이 더 있는 동안 반복
+            String line = sc.nextLine(); // 한 줄 읽기
+            System.out.println(line); // 그대로 출력
+        }
+        
+            }
+}
