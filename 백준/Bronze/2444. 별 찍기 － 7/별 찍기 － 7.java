9d3ffd7f248e7fd8c6e84ed4eb17b4bd38188bc1
bambee <124007041+bambee83@users.noreply.github.com>
2024-11-28 23:33:27 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 392 ms, Memory: 22040 KB -BaekjoonHub

---
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+
+        // 위쪽 삼각형 (1부터 N까지)
+        for (int i = 1; i <= N; i++) {
+            // 공백 출력
+            for (int j = 1; j <= N - i; j++) {
+                System.out.print(" ");
+            }
+            // 별 출력
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        // 아래쪽 삼각형 (N-1부터 1까지)
+        for (int i = N - 1; i >= 1; i--) {
+            // 공백 출력
+            for (int j = 1; j <= N - i; j++) {
+                System.out.print(" ");
+            }
+            // 별 출력
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+}
