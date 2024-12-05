9be527ca800210394ecb941de783e7f78842d956
bambee <124007041+bambee83@users.noreply.github.com>
2024-12-05 17:03:54 +0900

    [Bronze V] Title: 직사각형, Time: 176 ms, Memory: 17640 KB -BaekjoonHub

---
+++ 백준/Bronze/27323. 직사각형/직사각형.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner input = new Scanner(System.in);
+        int A = input.nextInt();
+        int B = input.nextInt();
+
+        System.out.println(A*B);
+
+    }
+}
