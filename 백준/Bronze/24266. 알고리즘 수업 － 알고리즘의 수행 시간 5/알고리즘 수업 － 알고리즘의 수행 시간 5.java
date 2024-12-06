cd960f74112ea269e38eb39aa93f5050f1f2d595
bambee <124007041+bambee83@users.noreply.github.com>
2024-12-06 16:31:08 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 5, Time: 172 ms, Memory: 17732 KB -BaekjoonHub

---
+++ 백준/Bronze/24266. 알고리즘 수업 － 알고리즘의 수행 시간 5/알고리즘 수업 － 알고리즘의 수행 시간 5.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner input = new Scanner(System.in);
+        Long n = input.nextLong();
+        System.out.println(n*n*n);
+        System.out.println(3);
+
+    }
+
+}
