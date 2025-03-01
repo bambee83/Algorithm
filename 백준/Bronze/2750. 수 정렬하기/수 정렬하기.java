3d3091740d40f8662e2e4b7dd4b63eeeb96c63b8
bambee <124007041+bambee83@users.noreply.github.com>
2025-03-01 16:02:33 +0900

    [Bronze II] Title: 수 정렬하기, Time: 140 ms, Memory: 14892 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -2,22 +2,17 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        Scanner scanner = new Scanner(System.in);
-
-        // 첫 번째 줄에서 숫자의 개수 N 입력 받음
-        int N = scanner.nextInt();
-        int[] numbers = new int[N];
-
-        // N개의 숫자를 입력 받아 배열에 저장
-        for (int i = 0; i < N; i++) {
-            numbers[i] = scanner.nextInt();
-        }
-        // 배열을 오름차순으로 정렬
-        Arrays.sort(numbers);
-        
-        for (int num : numbers) {
-            System.out.println(num);
-        }
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int N = Integer.parseInt(br.readLine());
+    int[] arr = new int[N];
+    for (int i = 0; i < N; i++) {
+      int a = Integer.parseInt(br.readLine());
+      arr[i] = a;
     }
+    Arrays.sort(arr);
+    for (int i : arr) {
+      System.out.println(i);
+    }
+  }
 }
