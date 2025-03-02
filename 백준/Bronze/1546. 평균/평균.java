e1f1226775a31abdfd461588cd911da2a8569af7
bambee <124007041+bambee83@users.noreply.github.com>
2025-03-02 21:36:49 +0900

    [Bronze I] Title: 평균, Time: 104 ms, Memory: 14236 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -1,26 +1,25 @@
+import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt(); // 시험 본 과목의 갯수
-        double[] scores = new double[N];
-
-        double maxScore = 0;
-        for (int i = 0; i < N; i++) {
-            scores[i] = sc.nextDouble();
-            if (scores[i] > maxScore) {
-                maxScore = scores[i];  // 최댓값 갱신
-            }
-        }
-        // 새로운 점수 계산
-        double sum = 0; // 변환 점수의 합
-        for (int i = 0; i < N; i++) {
-            sum += (scores[i] / maxScore) * 100;
-        }
-        // 새로운 평균 계산
-        double newAverage = sum / N;
-        System.out.println(newAverage);
-
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int n = Integer.parseInt(br.readLine());
+    int[] original = new int[n];
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    for (int i = 0; i < n; i++) {
+      original[i] = Integer.parseInt(st.nextToken());
     }
+    int max = original[0];
+    for (int i = 1; i < n; i++) {
+      if (max < original[i]) {
+        max = original[i];
+      }
+    }
+    double sum = 0;
+    for (int i = 0; i < n; i++) {
+      sum += (double) original[i] /max*100;
+    }
+    System.out.println(sum/n);
+  }
 }
